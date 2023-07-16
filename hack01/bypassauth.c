416b23f0c8782a5a5bbf236c933ae93e337e4594
Lucas Romero <lucas.romero@ifood.com.br>
2023-07-16 14:47:24 -0300

    add hack01

---
+++ hack01/bypassauth.c
@@ -0,0 +1,10 @@
+// Lucas de Medeiros França Romero, 11219154
+// Leonardo Antonetti da Motta, 11275338
+// Marcus Vinícius Santos Rodrigues, 11218862
+// Gabriel Monteiro Ferracioli, 11219129
+// André Santana Fernandes, 11208537
+
+
+int authorize () {
+    return 1;
+}
